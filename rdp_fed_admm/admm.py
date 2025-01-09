926bd77dc076e8b2d633ec9f51318ff5b5b2dde0
altr <csdp1353@csd.uoc.gr>
2025-01-09 12:17:11 +0200

    (feat) elastic aggregation
    Implements [1] using the terms of the dual gradient ascent as a measure
    of parameter sensitivity. The paper also implies a weighted aggregation
    so that has to be implemented as well.
    
    [1] https://openaccess.thecvf.com/content/CVPR2023/papers/Chen_Elastic_Aggregation_for_Federated_Optimization_CVPR_2023_paper.pdf

--- rdp_fed_admm/admm.py
+++ rdp_fed_admm/admm.py
@@ -1,7 +1,7 @@
 from logging import info
 from random import sample
 from select import select
-from typing import Self, override
+from typing import Self, cast, override
 
 import numpy as np
 from numpy.random import Generator
@@ -41,6 +41,7 @@ class ADMMClient(_ADMMBase, Client):
         step_size: float = 0.3,
         penalty_term: float = 0.6,
         clipping_threshold: float = 0.1,
+        sensitivity_momentum: float = 0.93,
         cache_factorizations: bool = True,
     ) -> None:
         _ADMMBase.__init__(self, seed, step_size, penalty_term)
@@ -51,6 +52,7 @@ class ADMMClient(_ADMMBase, Client):
         self._clip_thresh: float = clipping_threshold
         self._X: FArr | None = None
         self._Y: FArr | None = None
+        self._momentum: float = sensitivity_momentum
         self._use_cache: bool = cache_factorizations
         self._cache: dict[str, FArr] = {}
 
@@ -58,6 +60,14 @@ class ADMMClient(_ADMMBase, Client):
     def _clip(v: FArr, thresh: Float) -> FArr:
         scale: Float = np.min(np.array([thresh, np.linalg.norm(v)]))
         return v * scale
+
+    @staticmethod
+    def _edma(x: FArr, s: FArr, alpha: float) -> FArr:
+        """Exponentially decaying moving average.
+
+        Returns s' := αs + (1-α)x
+        """
+        return alpha * s + (1-alpha) * x
 
     def _cache_miss(self, key: str) -> bool:
         return key not in self._cache or not self._use_cache
@@ -77,6 +87,10 @@ class ADMMClient(_ADMMBase, Client):
         z: FArr = np.zeros_like(x)
         u: FArr = np.zeros_like(x)
         du: FArr = np.zeros_like(u)
+        sensitivity: FArr = np.zeros_like(x)
+
+        # local dataset size: used for weighting
+        self.send_array(np.array(X.shape[0]))
 
         for _ in range(n_iter):
             info("Waiting for z")
@@ -87,11 +101,12 @@ class ADMMClient(_ADMMBase, Client):
                 raise
 
             x = self._x_update(X, Y, 2 * z - u, u, z)
-
             rsd = self._clip(x - z, self._clip_thresh)
+            sensitivity = self._edma(np.abs(rsd), sensitivity, self._momentum)
             noise = 0.5 * self.rng.random(dim_weights)
             du = 2 * self._step * (rsd + noise)
 
+            self.send_array(sensitivity)
             self.send_array(du)
 
             u += du
@@ -111,12 +126,14 @@ class ADMMServer(_ADMMBase, Server):
         step_size: float = 0.3,
         penalty_term: float = 0.6,
         subset_size: float = 0.7,
+        elastic_boost: float = 0.5,
     ) -> None:
         _ADMMBase.__init__(self, seed, step_size, penalty_term)
         Server.__init__(self, addr, port, max_clients)
         self._subset_size: float = subset_size
         self._n_clients: int
         self._coeffs: FArr | None
+        self._boost: float = elastic_boost
 
     def _z_update(self, z: FArr) -> FArr:
         raise NotImplementedError("This is meant to be overridden")
@@ -135,11 +152,21 @@ class ADMMServer(_ADMMBase, Server):
         self.activate_server()
         self._n_clients = len(self._client_conn)
 
-        for _ in range(n_iter):
-            subset = sample(
-                list(self._client_conn.values()),
-                int(self._n_clients * self._subset_size),
-            )
+        senses = np.zeros((self._n_clients, dim_weights[0]))
+
+        info("Collecting dataset sizes from clients")
+        data_weights = []
+        for client in self._client_conn.values():
+            data_weights.append(self.recv_array(client))
+        data_weights = np.array(data_weights).astype(float)
+        data_weights /= data_weights.sum()
+        data_weights = cast(FArr, data_weights)
+
+        conns = list(self._client_conn.values())
+
+        for i in range(n_iter):
+            print(i)
+            subset = sample(conns, int(self._n_clients * self._subset_size))
             nsubs = len(subset)
 
             for fd in subset:
@@ -152,10 +179,18 @@ class ADMMServer(_ADMMBase, Server):
                     raise RuntimeError
 
                 for fd in rfds:
-                    du += self.recv_array(fd)
+                    idx = conns.index(fd)
+                    weight = data_weights[idx]
+                    senses[idx,:] = self.recv_array(fd).ravel()
+                    senses[idx,:] *= weight
+                    du += weight * self.recv_array(fd)
                     subset.remove(fd)
 
-            du /= nsubs
+            agg_senses = senses.sum(axis=0)  # agg over clients
+            agg_senses /= agg_senses.max()   # normalize
+            elastic_weights = 1 + self._boost - agg_senses
+            du *= np.atleast_2d(elastic_weights).T
+            # du /= nsubs
             z = self._z_update(du)
 
         self._coeffs = z
