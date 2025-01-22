3c13a2dfecf8f82793f4245228e8666d6d14ceb4
altr <csdp1353@csd.uoc.gr>
2025-01-22 22:23:43 +0200

    (feat) new parser
    The argument parser was starting to show its age as more things kept
    getting added. The new implementation makes use of some more advanced
    functionalities of `argparse` and should generally be more
    well-structured.

--- rdp_fed_admm/rdp_fed_admm.py
+++ rdp_fed_admm/rdp_fed_admm.py
@@ -1,58 +1,19 @@
 import logging
 from typing import cast
 
-import numpy as np
-
-from ._parser import Args, parser
-from .objectives.elasticnet import ElasticNetADMMClient, ElasticNetADMMServer
+from ._args import Args, parser
 
 
 def rdp_fed_admm():
-    args = Args()
-    args = parser.parse_args(namespace=args)
-
+    args = cast(Args, parser.parse_args())
     log = logging.getLogger(__name__)
-
-    fdata: str = args.dataset
-    n_features: int = args.features
-    target_col: int = args.target
-    n_iter: int = args.n_iter
-
-    data = np.genfromtxt(fdata, delimiter=",", skip_header=1)
-
-    # for i, col in enumerate(data.T):
-    #     data[:, i] = (col - col.mean()) / col.std(ddof=2)
-
-    Y = data[:, target_col]
-    X = np.delete(data, target_col, axis=1)
 
     logging.basicConfig(format="[%(levelname)s] %(asctime)s %(message)s",
                         datefmt='%m/%d/%Y %I:%M:%S %p',
                         level=logging.INFO if args.verbose else logging.WARNING,
-                        filename=cast(str | None,  args.log_file))
+                        filename=cast(str | None, args.log_file))
 
-    if args.client:
-        log.info(f"Registering client for {args.address}:{args.port}")
-        with ElasticNetADMMClient(
-            args.address,
-            args.port,
-            loss="mae",
-            n_iter=n_iter,
-        ) as cli:
-            _ = cli.fit(X, Y)
-    elif args.server:
-        log.info(f"Registering server for {args.address}:{args.port}")
-        with ElasticNetADMMServer(
-            args.address,
-            args.port,
-            max_clients=2,
-            n_iter=n_iter,
-        ) as srv:
-            log.info(f"Listening on {args.address}:{args.port}")
-            preds = srv.fit(n_features).predict(X)
-            log.warning("MAE: {:.3f}".format(np.abs(preds - Y).mean()))
-    else:
-        raise RuntimeError("Undefined behaviour")
+    args.func(args, log)
 
 
 if __name__ == "__main__":
